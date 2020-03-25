a94854a66ea4549606602ad1beb700a97fb84143
al1k24 <al1k24@icloud.com>
2020-03-26 00:31:08 +0200

    Add MySQL DataBase service

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,19 @@
+import alik.master.service.DataBaseService;
 import alik.master.service.PhoneBookService;
+
+import java.sql.SQLException;
 
 public class Main {
 
     public static void main(String[] args) {
+        DataBaseService dbs = new DataBaseService();
+        try {
+            dbs.createTableIfNotExists();
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+            return;
+        }
+
         PhoneBookService.start();
     }
 }
