230e74af2c1b9dec0c6434c9b221369431fd71ac
al1k24 <al1k24@icloud.com>
2020-03-26 00:19:42 +0200

    Add DataBase configuration file

---
+++ src/alik/master/db/Config.java
@@ -0,0 +1,10 @@
+package alik.master.db;
+
+public interface Config {
+
+    String HOST = "31.31.196.81";
+    String PORT = "3306";
+    String DATABASE = "u0533070_pb";
+    String USER = "u0533070_pb";
+    String PASSWORD = "Z0o0B9h5";
+}
