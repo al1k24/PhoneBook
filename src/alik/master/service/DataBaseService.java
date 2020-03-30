77a99c7a9bcce8d54121f782066df78d5bf96b62
al1k24 <al1k24@icloud.com>
2020-03-30 21:21:04 +0300

    Add editContact & getContacts functions

--- src/alik/master/service/DataBaseService.java
+++ src/alik/master/service/DataBaseService.java
@@ -4,7 +4,10 @@ import alik.master.model.Contact;
 import alik.master.model.DataBase;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class DataBaseService {
 
@@ -32,6 +35,29 @@ public class DataBaseService {
         return ps.executeUpdate() > 0;
     }
 
+    public boolean editContact(int id, int choice, String value) throws ClassNotFoundException, SQLException {
+        String request;
+        switch (choice) {
+            case 1:
+                request = "UPDATE `contacts` SET `name`=? WHERE `id`=?";
+                break;
+            case 2:
+                request = "UPDATE `contacts` SET `surname`=? WHERE `id`=?";
+                break;
+            case 3:
+                request = "UPDATE `contacts` SET `phone`=? WHERE `id`=?";
+                break;
+            default:
+                throw new IllegalStateException("Unexpected value: " + choice);
+        }
+
+        PreparedStatement ps = db.getConnection().prepareStatement(request);
+        ps.setString(1, value);
+        ps.setInt(2, id);
+
+        return ps.executeUpdate() > 0;
+    }
+
     public boolean removeContact(int id) throws ClassNotFoundException, SQLException {
         String request = "DELETE FROM `contacts` WHERE `id` = ?";
 
@@ -39,5 +65,28 @@ public class DataBaseService {
         ps.setInt(1, id);
 
         return ps.executeUpdate() > 0;
+    }
+
+    public Map<Integer, Contact> getContacts() throws ClassNotFoundException, SQLException {
+        Map<Integer, Contact> contacts = new HashMap<>();
+
+        String request = "SELECT * FROM `contacts`";
+
+        PreparedStatement ps = db.getConnection().prepareStatement(request);
+
+        int id;
+        String name, surname, phone;
+
+        ResultSet result = ps.executeQuery();
+        while (result.next()) {
+            id = result.getInt("id");
+            name = result.getString("name");
+            surname = result.getString("surname");
+            phone = result.getString("phone");
+
+            contacts.put(id, new Contact(name, surname, phone));
+        }
+
+        return contacts;
     }
 }
