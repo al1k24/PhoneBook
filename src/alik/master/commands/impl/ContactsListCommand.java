75e2268b424eb8f512b66cc85986ea2b662edb40
al1k24 <al1k24@icloud.com>
2020-03-30 21:22:45 +0300

    Update getContacts function

--- src/alik/master/commands/impl/ContactsListCommand.java
+++ src/alik/master/commands/impl/ContactsListCommand.java
@@ -2,15 +2,22 @@ package alik.master.commands.impl;
 
 import alik.master.commands.CommandExecution;
 import alik.master.model.Contact;
-import alik.master.service.PhoneBookService;
+import alik.master.service.DataBaseService;
 
+import java.sql.SQLException;
 import java.util.Map;
 
 public class ContactsListCommand implements CommandExecution {
 
     @Override
     public void execute() {
-        Map<Integer, Contact> contacts = PhoneBookService.getContacts();
-        contacts.forEach((id, contact) -> System.out.println("ID: " + id + " | " + contact.getFullName() + " Tel: " + contact.getPhone()));
+        try {
+            DataBaseService dbs = new DataBaseService();
+            Map<Integer, Contact> contacts = dbs.getContacts();
+
+            contacts.forEach((id, contact) -> System.out.println("ID: " + id + " | " + contact.getFullName() + " Tel: " + contact.getPhone()));
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
